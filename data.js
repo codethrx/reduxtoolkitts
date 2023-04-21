8bc36eb9c832dbfe53df36a4addce8bb9b40729d
codethrx <tatheermehdi2000@gmail.com>
2023-04-22 00:11:27 +0500

    data.js changed

--- data.js
+++ data.js
@@ -3,9 +3,6 @@ const GAMES = [
   { name: "COD-MW2", rating: 8 },
   { name: "GTA-v", rating: 8.5 },
   { name: "COD-MW2", rating: 8 },
-  { name: "new content added", rating: 9 },
-  { name: "new content added", rating: 9 },
-  { name: "new content added", rating: 9 },
-  { name: "new content added", rating: 9 },
+  { go: true },
   { name: "new content added", rating: 9 },
 ];
